bca3e0cdd66c2ae86de3ede6425bafd4dd12672b
rizwanawanfb <133207837+rizwanawanfb@users.noreply.github.com>
2024-02-24 15:07:58 +0000

    Conversion of data types

---
+++ 01-Basics/Variable.js
@@ -0,0 +1,12 @@
+const accountID = 1258;
+let accountEmail = "rizwan@google.com";
+var accountPassword = "12345";
+accountCIty = "Karachi";
+
+// accountID = 1111; This is not allowed
+
+accountEmail = "rizwaaan@google.com";
+accountPassword = 11111;
+accountCIty = "Mansehra";
+
+console.table([accountEmail, accountPassword, accountCIty]);
