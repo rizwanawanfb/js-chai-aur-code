7107a9c5858fba11bb82e4fa8be243484db7bf39
rizwanawanfb <133207837+rizwanawanfb@users.noreply.github.com>
2024-02-24 17:48:16 +0000

    Data type last class

--- 01-Basics/conversionOperators.js
+++ 01-Basics/conversionOperators.js
@@ -22,5 +22,26 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let myNumberIs = 33;
 
 let converMyNumber =  String(myNumberIs)
-console.log(converMyNumber);
-console.log(typeof converMyNumber);
+// console.log(converMyNumber);
+// console.log(typeof converMyNumber);
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
---
+++ 01-Basics/datatype_summary.js
@@ -0,0 +1,25 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.31;
+
+
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+// console.log(id === anotherId);
+
+const heros = ["Rizwan", "Muzammil", "Hassan"];
+let myObj = {
+    name: "Rizwan",
+    age: "18",
+}
+
+const myFunction = function (){
+    console.log("Hello world!");
+}
+
+console.log(typeof anotherId);
