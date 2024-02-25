45f0c3ec4e74c2b985ad9d866486e08dabefae6a
rizwanawanfb <133207837+rizwanawanfb@users.noreply.github.com>
2024-02-25 06:42:35 +0000

    New changes

---
+++ 01-Basics/string.js
@@ -0,0 +1,7 @@
+const sentence = 'The quick brown fox jumps over th elady log';
+
+let index = 5;
+
+console.log(`An index of ${index} return the character ${sentence.at(index)}`);
+
+// An index of 5 retun the character u;
---
+++ 02-Basics/01_Arrays.js
@@ -0,0 +1,22 @@
+const myArray = [1, 2, 3, 4, 5]
+
+// console.log(myArray[2])
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop(7)
+
+// myArray.unshift(0);
+// myArray.shift()
+
+// myArray.includes(5)
+// console.log(myArray.includes(2));
+// console.log(myArray.indexOf(3));
+
+// const newArray = myArray.join()
+
+// console.log(myArray);
+// console.log(newArray);
+
+// slice, splice
+console.log("A", myArray);
